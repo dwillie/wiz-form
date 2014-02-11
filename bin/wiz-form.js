f193774964a2b55bd3a648fc40a521cff9c04aa8
David Willie <dwillie@swin.edu.au>
2014-02-11 11:19:59 +1100

    initial commit

---
+++ bin/wiz-form.js
@@ -0,0 +1,113 @@
+angular.module('asset.common.partials.wiz', []).directive('wizForm', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'common/partials/templates/wiz-form.tpl.html',
+    transclude: true,
+    replace: true,
+    scope: {
+      onFinish: '&',
+      wizardMeta: '='
+    },
+    controller: [
+      '$scope',
+      function ($scope) {
+        $scope.steps = [];
+        $scope.currentStep = 0;
+        this.registerStep = function (stepObject) {
+          $scope.steps.push(stepObject);
+          if ($scope.wizardMeta) {
+            $scope.wizardMeta.totalSteps = $scope.steps.length;
+          }
+          return $scope.steps.length - 1;
+        };
+        this.getCurrentStep = function () {
+          return $scope.currentStep;
+        };
+        $scope.nextText = function () {
+          if ($scope.steps[$scope.currentStep] && $scope.steps[$scope.currentStep].next_text) {
+            return $scope.steps[$scope.currentStep].next_text;
+          } else {
+            return false;
+          }
+        };
+        $scope.previousText = function () {
+          if ($scope.steps[$scope.currentStep] && $scope.steps[$scope.currentStep].previous_text) {
+            return $scope.steps[$scope.currentStep].previous_text;
+          } else {
+            return false;
+          }
+        };
+        $scope.isError = function () {
+          return $scope.message != null && $scope.message.error != null;
+        };
+        $scope.previousStep = function () {
+          if ($scope.currentStep > 0) {
+            return $scope.currentStep -= 1;
+          }
+        };
+        $scope.lastStep = function () {
+          return $scope.currentStep >= $scope.steps.length - 1;
+        };
+        $scope.nextStep = function () {
+          var readyCheck;
+          readyCheck = $scope.steps[$scope.currentStep].ready_check;
+          if (readyCheck != null) {
+            $scope.message = readyCheck();
+          }
+          if ($scope.isError()) {
+            return;
+          }
+          $scope.message = null;
+          if ($scope.lastStep()) {
+            if ($scope.onFinish) {
+              return $scope.onFinish();
+            }
+          } else {
+            return $scope.currentStep += 1;
+          }
+        };
+        $scope.$watch('currentStep', function () {
+          if ($scope.wizardMeta && $scope.steps[$scope.currentStep] && $scope.steps[$scope.currentStep].name) {
+            $scope.wizardMeta.activeStep = $scope.currentStep;
+            return $scope.wizardMeta.activeStepName = $scope.steps[$scope.currentStep].name;
+          }
+        });
+        return this;
+      }
+    ]
+  };
+}).directive('wizStep', function () {
+  return {
+    restrict: 'E',
+    template: '<div><div ng-transclude></div></div>',
+    transclude: true,
+    replace: true,
+    require: '^wizForm',
+    scope: {
+      readyCheck: '&',
+      nextText: '@',
+      previousText: '@'
+    },
+    link: function ($scope, elem, attrs, wizFormCtrl) {
+      var stepIndex;
+      if (!attrs.readyCheck) {
+        $scope.readyCheck = null;
+      }
+      stepIndex = wizFormCtrl.registerStep({
+        name: attrs.name,
+        element: elem,
+        ready_check: $scope.readyCheck,
+        next_text: $scope.nextText,
+        previous_text: $scope.previousText
+      });
+      $scope.wizFormCtrl = wizFormCtrl;
+      return $scope.$watch('wizFormCtrl.getCurrentStep()', function () {
+        if (wizFormCtrl.getCurrentStep() === stepIndex) {
+          return elem.show();
+        } else {
+          return elem.hide();
+        }
+      });
+    }
+  };
+});
---
+++ src/wiz-form.coffee
@@ -0,0 +1,90 @@
+angular.module("asset.common.partials.wiz", [])
+
+.directive "wizForm", ->
+  restrict: "E"
+  templateUrl: "common/partials/templates/wiz-form.tpl.html"
+  transclude: true
+  replace: true
+  scope: {
+    onFinish: "&"
+    wizardMeta: "="
+  }
+  controller: ($scope) ->
+    $scope.steps       = []
+    $scope.currentStep = 0
+
+    @registerStep = (stepObject) ->
+      $scope.steps.push stepObject
+      if $scope.wizardMeta
+        $scope.wizardMeta.totalSteps = $scope.steps.length
+
+      return $scope.steps.length - 1
+
+    @getCurrentStep = ->
+      $scope.currentStep
+
+    $scope.nextText = ->
+      if $scope.steps[$scope.currentStep] && $scope.steps[$scope.currentStep].next_text
+        $scope.steps[$scope.currentStep].next_text
+      else
+        false
+    $scope.previousText = ->
+      if $scope.steps[$scope.currentStep] && $scope.steps[$scope.currentStep].previous_text
+        $scope.steps[$scope.currentStep].previous_text
+      else
+        false
+
+    $scope.isError = ->
+      $scope.message? && $scope.message.error?
+    $scope.previousStep = ->
+      $scope.currentStep -= 1 if $scope.currentStep > 0
+    $scope.lastStep = ->
+      $scope.currentStep >= $scope.steps.length - 1
+    $scope.nextStep = ->
+      readyCheck = $scope.steps[$scope.currentStep].ready_check
+      if readyCheck?
+        $scope.message = readyCheck()
+      if $scope.isError()
+        return
+
+      $scope.message = null
+      if $scope.lastStep()
+        if $scope.onFinish
+          $scope.onFinish()
+      else
+        $scope.currentStep += 1
+
+    $scope.$watch 'currentStep', ->
+      if $scope.wizardMeta && $scope.steps[$scope.currentStep] && $scope.steps[$scope.currentStep].name
+        $scope.wizardMeta.activeStep     = $scope.currentStep
+        $scope.wizardMeta.activeStepName = $scope.steps[$scope.currentStep].name
+
+    return this
+
+.directive "wizStep", ->
+  restrict:    "E"
+  templateUrl: "common/partials/templates/wiz-step.tpl.html"
+  transclude:  true
+  replace:     true
+  require: "^wizForm"
+  scope:
+    readyCheck:   "&"
+    nextText:     "@"
+    previousText: "@"
+  link: ($scope, elem, attrs, wizFormCtrl) ->
+    unless attrs.readyCheck
+      $scope.readyCheck = null
+
+    stepIndex = wizFormCtrl.registerStep({
+      name:          attrs.name,
+      element:       elem,
+      ready_check:   $scope.readyCheck
+      next_text:     $scope.nextText
+      previous_text: $scope.previousText
+    })
+    $scope.wizFormCtrl = wizFormCtrl
+    $scope.$watch "wizFormCtrl.getCurrentStep()", ->
+      if wizFormCtrl.getCurrentStep() == stepIndex
+        elem.show()
+      else
+        elem.hide()
